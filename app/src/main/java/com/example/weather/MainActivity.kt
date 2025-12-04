01a8f47a9673d9cba3839198bf57add31b40d9f8
Abiezer Quiles <130618821+AbiQuiles@users.noreply.github.com>
2025-12-04 11:55:34 -0500

    refactor: Remove commented-out code
    Description:
    - Removed a commented-out URL from `MainActivity.kt`. This URL was a test example API endpoint and included a hardcoded API key, which is a security risk.

--- app/src/main/java/com/example/weather/MainActivity.kt
+++ app/src/main/java/com/example/weather/MainActivity.kt
@@ -25,6 +25,3 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
-
-
-//https://api.openweathermap.org/data/2.5/forecast/daily?q=lisbon&appid=ed60fcfbd110ee65c7150605ea8aceea&units=imperial
